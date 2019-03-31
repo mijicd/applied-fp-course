b15b4652edc38c18466caa0e7fdadb22194ae6e2
Dejan Mijic <dmijic@acm.org>
2019-03-31 17:52:09 +0200

    Complete level 1

--- src/Level01/Core.hs
+++ src/Level01/Core.hs
@@ -2,10 +2,8 @@
 {-# OPTIONS_GHC -fno-warn-unused-matches #-}
 module Level01.Core (runApp) where
 
-import           Network.Wai              (Application, Request, Response,
-                                           ResponseReceived, responseLBS)
+import           Network.Wai              (Application, responseLBS)
 import           Network.Wai.Handler.Warp (run)
-
 import           Network.HTTP.Types       (status200)
 
 -- Our "application" will respond to ALL incoming requests with a 200
@@ -28,12 +26,9 @@ import           Network.HTTP.Types       (status200)
 -- what you need.
 --
 -- We've used the non-synonym version of the `Application` type below.
-app
-  :: Request
-  -> (Response -> IO ResponseReceived)
-  -> IO ResponseReceived
+app :: Application
 app _ cb =
-  error "Application not implemented"
+  cb $ responseLBS status200 [("Content-Type", "text/plain")] "Hello, World!"
 
 -- We keep this main function here as it is useful to build your application as
 -- a library. The reasoning behind this is that when you come to do your
@@ -41,4 +36,4 @@ app _ cb =
 -- needing to worry about any initialisation code you've buried in your
 -- executable Main.hs.
 runApp :: IO ()
-runApp = run undefined undefined
+runApp = run 8080 app
